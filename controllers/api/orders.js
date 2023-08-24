4622fc221d1d0adb9055193e3fb4f38c0778c891
priyanga_pas <apriyangasingh@gmail.com>
2023-08-24 11:23:17 -0700

    initial

--- controllers/api/orders.js
+++ controllers/api/orders.js
@@ -1,4 +1,4 @@
-// const Order = require('../../models/order');
+const Order = require('../../models/order');
 // const Item = require('../../models/item');
 
 module.exports = {
@@ -6,23 +6,40 @@ module.exports = {
   addToCart,
   setItemQtyInCart,
   checkout,
+  history
 };
 
-// A cart is the unpaid order for a user
 async function cart(req, res) {
-
+  const cart = await Order.getCart(req.user._id);
+  res.json(cart);
 }
 
 // Add an item to the cart
 async function addToCart(req, res) {
-
+  const cart = await Order.getCart(req.user._id);
+  await cart.addItemToCart(req.params.id);
+  res.json(cart);
 }
 
 // Updates an item's qty in the cart
 async function setItemQtyInCart(req, res) {
+  const cart = await Order.getCart(req.user._id);
+  await cart.setItemQty(req.body.itemId, req.body.newQty);
+  res.json(cart);
 }
 
 // Update the cart's isPaid property to true
 async function checkout(req, res) {
-
+  const cart = await Order.getCart(req.user._id);
+  cart.isPaid = true;
+  await cart.save();
+  res.json(cart);
 }
+
+async function history(req,res) {try{const userId=req.user._id;
+const orders=await Order.find({user:userId, isPaid:true});
+res.json(orders);
+}
+catch {
+  res.status(400).json("order fetch issue");
+}}
